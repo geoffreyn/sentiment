72370cb6045f6ffd671dbbb7fcdf3c0636ac4306
Geoffrey Newman <geoffrey.newman@qeexo.com>
2019-06-15 16:49:38 +0800

    Fix indexing error and add CV boxplot

--- solution.py
+++ solution.py
@@ -10,6 +10,7 @@ Terminal Arguments:
 import sys
 import warnings
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import nltk
@@ -17,9 +18,11 @@ from sklearn.model_selection import KFold
 
 nltk.download('punkt')
 
-# TODO investigate case-sensitivity: sparser but capitlization can be indicative of emotional weight
 # TODO word stemming and other techniques to reduce dimensionality of input space
 # TODO try weighing word sentiment by length (longer words should carry more emotion)
+# TODO replace words with synonyms to reduce dimensionality
+# TODO correct typos and expand abbreviations
+# TODO investigate case-sensitivity: sparser but capitlization can be indicative of emotional weight
 str_splitter = lambda str_: [s.lower() for s in nltk.tokenize.word_tokenize(str_)]
 accuracy = lambda df : (np.sum(df['prediction'] == df['sentiment']) /
                         len(df) * 100)
@@ -30,12 +33,14 @@ n_folds = 5
 
 
 def word_to_dict_key(wordlist: pd.Series):
+    print('discovering words...', end=' ')
     blank_dict = {}
     for row in wordlist:
         for word in str_splitter(row):
             if word not in blank_dict:
                 blank_dict[word] = 0
 
+    print('Done!')
     return blank_dict
 
 
@@ -120,9 +125,8 @@ class Sentiment(object):
         df_len = len(df)
 
         predicted_sentiment_vect = [0] * df_len # Preallocating should be
-                                                 # more efficient
-                                                 # than adding each element
-                                                 # one at a time
+                                                # more efficient than
+                                                # growing psv and psvs
         predicted_sentiment_vect_string = [0] * df_len
 
         for row in df.itertuples():
@@ -130,13 +134,17 @@ class Sentiment(object):
             if num == df_len:
                 break
 
-            predicted_sentiment_vect[num] = self.eval_sentiment(sent_dict,
-                                                                row.content)
+            try:
+                predicted_sentiment_vect[num] = self.eval_sentiment(sent_dict,
+                                                                    row.content)
 
-            predicted_sentiment_vect_string[num] = (
-                'positive' if predicted_sentiment_vect[num] > 0 else (
-                    'negative' if predicted_sentiment_vect[num] < 0 else 'neutral')
-            )
+                predicted_sentiment_vect_string[num] = (
+                    'positive' if predicted_sentiment_vect[num] > 0 else (
+                        'negative' if predicted_sentiment_vect[num] < 0 else 'neutral')
+                )
+            except IndexError:
+                # Empty content?
+                continue
 
         df['prediction_int'] = predicted_sentiment_vect
         df['prediction'] = predicted_sentiment_vect_string
@@ -161,24 +169,20 @@ def main(argv):
 
     twitter_sentiment = Sentiment(wc_dict=blank_dict, sent_dict=blank_dict,
                                   sent_dict_wcr=blank_dict)
-
-    # Convert possible sentiment output strings to numerical values
-    train_df['sentiment_int'] = train_df['sentiment'].map(basic_dict)
-    test_df['sentiment_int'] = test_df['sentiment'].map(basic_dict)
-
     # Fit into sklearn syntax
-    X, y = train_df['content'], train_df['sentiment_int']
+    X, y = train_df['content'], train_df['sentiment']
 
     kf = KFold(n_splits=n_folds, shuffle=True)  # Define the split - into 2
     # folds
     kf.get_n_splits(X)
 
-    fold_accuracy = [0] * n_folds
+    fold_train_accuracy = [0] * n_folds
+    fold_test_accuracy = [0] * n_folds
 
     sentiment_list = [Sentiment(wc_dict=blank_dict, sent_dict=blank_dict,
                                   sent_dict_wcr=blank_dict)] * n_folds
     for fold, (train_index, test_index) in enumerate(kf.split(X)):
-        print('Working on fold {}'.format(fold+1))
+        print('Working on fold {}: '.format(fold+1), end=' ')
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
@@ -186,36 +190,47 @@ def main(argv):
         subdf_train = pd.concat([X_train, y_train], axis=1)
         subdf_test = pd.concat([X_test, y_test], axis=1)
 
+        # Convert possible sentiment output strings to numerical values
+        subdf_train['sentiment_int'] = subdf_train['sentiment'].map(basic_dict)
+        subdf_test['sentiment_int'] = subdf_test['sentiment'].map(basic_dict)
+
         iterator = subdf_train.copy().itertuples()
         # Train on training folds
+        print('Fitting...', end=' ')
         for row in iterator:
             sentiment_list[fold].fit(regularize=regularize,
                                      content=row.content,
                                      sentiment_int=row.sentiment_int)
 
+        print('Regularizing...', end=' ')
         iterator = subdf_train.copy().itertuples()
         for row in iterator:
             sentiment_list[fold].wc_fit(regularize=regularize,
                                         content=row.content,
                                         sentiment_int=row.sentiment_int)
 
+        print('Done!')
         # Evaluate on train then test folds
         sentiment_list[fold].evaluate(subdf_train,
                                       sentiment_list[fold].sent_dict_wcr)
 
+        fold_train_accuracy[fold] = accuracy(subdf_train)
         print("Fold {} accuracy{} on training set: {:0.2f}%".
                   format(fold+1, ' with regularization' if regularize else '',
-                         accuracy(subdf_train)))
+                         fold_train_accuracy[fold]))
 
         sentiment_list[fold].evaluate(subdf_test,
                                       sentiment_list[fold].sent_dict_wcr)
-        fold_accuracy[fold] = accuracy(subdf_test)
+        fold_test_accuracy[fold] = accuracy(subdf_test)
         print("Fold {} accuracy{} on test set: {:0.2f}%".
                   format(fold+1, ' with regularization' if regularize else '',
-                         fold_accuracy[fold]))
+                         fold_test_accuracy[fold]))
 
-    print("{} fold cross-validation accuracy: {:0.2f}".format(n_folds,
-                                                              np.mean(fold_accuracy)))
+    print("{} fold cross-validation average accuracy: {:0.2f}%".
+              format(n_folds, np.mean(fold_test_accuracy)))
+
+    train_df['sentiment_int'] = test_df['sentiment'].map(basic_dict)
+    test_df['sentiment_int'] = test_df['sentiment'].map(basic_dict)
 
     iterator = train_df.copy().itertuples()
     ## Train the full model
@@ -241,6 +256,10 @@ def main(argv):
               format(' with regularization' if regularize else '',
                      accuracy(test_df)))
 
+    plt.boxplot([fold_train_accuracy, fold_test_accuracy],
+                labels=['Training', 'Test']);
+    plt.title('Cross-validation accuracy')
+    plt.show()
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
